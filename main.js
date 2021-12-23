e19d72b1c0d528df2a564247dd8cd1cbb6476f87
Arnice123 <91489078+Arnice123@users.noreply.github.com>
2021-12-23 09:52:09 -0800

    added comments to help describe the code

--- main.js
+++ main.js
@@ -5,22 +5,26 @@ var roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function () {
 
+
    for(var name in Memory.creeps) {
-        if(!Game.creeps[name]) { 
+        if(!Game.creeps[name]) {
             delete Memory.creeps[name];
             console.log('Clearing non-existing creep memory:', name);
         }
     }
 
+    // getting the creeps with the harvester role
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     console.log('Harvesters: ' + harvesters.length);
 
+    // if there are less than 2 harvesters spawn a new one
     if(harvesters.length < 2) {
         var newName = 'Harvester' + Game.time;
         console.log('Spawning new harvester: ' + newName);
         Game.spawns['Arnice123 First'].spawnCreep([WORK,WORK,MOVE,CARRY,CARRY], newName,
             {memory: {role: 'harvester'}});
-            
+
+        // if the harvesters didn't farm enough energy use this so there will allways be harvesters
         if (harvesters.length == 0)
         {
             Game.spawns['Arnice123 First'].spawnCreep([WORK,MOVE,CARRY], newName,
@@ -28,15 +32,18 @@ module.exports.loop = function () {
         }
     }
 
+    // getting the creeps with the upgrader role
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
     console.log('Upgraders: ' + upgraders.length);
 
+    // if there are less than 2 upgraders spawn a new one
     if(upgraders.length < 2) {
         var newName = 'Upgrader' + Game.time;
         console.log('Spawning new upgrader: ' + newName);
         Game.spawns['Arnice123 First'].spawnCreep([WORK,WORK,WORK,CARRY,MOVE], newName,
             {memory: {role: 'upgrader'}});
-            
+
+        // if the harvesters didn't farm enough energy use this so there will allways be some upgrades even if they are not good
         if (upgraders.length == 0)
         {
             Game.spawns['Arnice123 First'].spawnCreep([WORK,MOVE,CARRY], newName,
@@ -44,15 +51,18 @@ module.exports.loop = function () {
         }
     }
 
+    // getting the creeps with the builders role
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
     console.log('Builders: ' + builders.length);
 
+    // if there are less than 2 builders spawn a new one
     if(builders.length < 2) {
         var newName = 'Builders' + Game.time;
         console.log('Spawning new builders: ' + newName);
         Game.spawns['Arnice123 First'].spawnCreep([WORK,CARRY,WORK,MOVE], newName,
             {memory: {role: 'builder'}});
-            
+
+        // if the harvesters didn't farm enough energy use this so there will not be a lack of creeps
         if (builders.length == 0)
         {
             Game.spawns['Arnice123 First'].spawnCreep([WORK,MOVE,CARRY], newName,
@@ -60,6 +70,7 @@ module.exports.loop = function () {
         }
     }
 
+    // displaying what is spawning
     if(Game.spawns['Arnice123 First'].spawning) {
         var spawningCreep = Game.creeps[Game.spawns['Arnice123 First'].spawning.name];
         Game.spawns['Arnice123 First'].room.visual.text(
@@ -81,14 +92,14 @@ module.exports.loop = function () {
             roleBuilder.run(creep);
         }
     }
-    /*
+/*
      // Modulo - 10 ticks
-    const waitTime = 10; 
-    
+    const waitTime = 10;
+
     // If the remainder of dividing the current game time by some value is 0, then its been some value of ticks
     if(Game.time%waitTime == 0){
         console.log("hi")
         FindEmptySites()
     }
-    */
+*/
 }
