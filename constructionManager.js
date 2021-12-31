fa880fd6618ddf523df63dd2900a305db150bbda
Arnice123 <91489078+Arnice123@users.noreply.github.com>
2021-12-31 12:08:58 -0800

    Update constructionManager.js

--- constructionManager.js
+++ constructionManager.js
@@ -3,51 +3,54 @@
 */
 
     // create a path from the spawn to the energy sources
-    const path1 = Game.spawns[0].pos.findPathTo(Game.flags.SOURCE1.pos)
-    const path2 = Game.spawns[0].pos.findPathTo(Game.flags.SOURCE2.pos)
-
-    // Serilizing the paths
-    Memory.path1 = Game.room.serializePath(path1)
-    Memory.path2 = Game.room.serializePath(path2)    
+    const path1 = Game.spawns['Arnice123 First'].pos.findPathTo(Game.flags.SOURCE1.pos)
+    const path2 = Game.spawns['Arnice123 First'].pos.findPathTo(Game.flags.SOURCE2.pos)
+    
+    
 
 // function that will be called every 25 tics in order to find missing paths
-function FindEmptySites(creep)
+function FindEmptySites()
 {
-    // Getting the room terrain
-    const terrain = Game.rooms['E32N8'].getTerrain()
+    const constructSitesLength = Game.spawns['Arnice123 First'].room.find(FIND_MY_STRUCTURES, {
+    filter: { structureType: FIND_MY_CONSTRUCTION_SITES }
+    
+    if (constructSitesLength.length >= 100) 
+    {
+        return
+    }
+    
+      // Getting the room terrain
+    var terrain = Game.rooms['E32N8'].getTerrain()
     
     // for each position in the path there should be a road
-    for (var pos in Memory.path1)
+    for (var pos in path1)
     {
-       switch(terrain.get(Memory.path1[pos].x, Memory.path1[pos].y))
+       switch(terrain.get(path1[pos].x, path1[pos].y))
        {
             // if there is floor or a swamp place a road on that position
             case TERRAIN_MASK_SWAMP:
-                Game.rooms.sim.createConstructionSite(Memory.path1[pos].x, Memory.path1[pos].y, STRUCTURE_ROAD)
+                Game.path1[pos].pos.createConstructionSite(STRUCTURE_ROAD)
                 break
             case 0:
-                Game.rooms.sim.createConstructionSite(Memory.path1[pos].x, Memory.path1[pos].y, STRUCTURE_ROAD)
+                Game.path1[pos].pos.createConstructionSite(STRUCTURE_ROAD)
                 break
        }
     }
 
     //same thing as previously
-    for (var pos in Memory.path2)
+    for (var pos in path2)
     {
-       switch(terrain.get(Memory.path2[pos].x, Memory.path2[pos].y))
+       switch(terrain.get(path2[pos].x, path2[pos].y))
        {
             case TERRAIN_MASK_SWAMP:
-                Game.rooms.sim.createConstructionSite(Memory.path2[pos].x, Memory.path2[pos].y, STRUCTURE_ROAD)
+                Game.path2[pos].pos.createConstructionSite(STRUCTURE_ROAD)
                 break
             case 0:
-                Game.rooms.sim.createConstructionSite(Memory.path2[pos].x, Memory.path2[pos].y, STRUCTURE_ROAD)
+                Game.path2[pos].pos.createConstructionSite(STRUCTURE_ROAD)
                 break
        }
     }
 
 }
-
-
-
 
 module.exports = FindEmptySites()
