ff75d4333ced281adf6f67cd256233d9a68daaf7
Arnice123 <91489078+Arnice123@users.noreply.github.com>
2022-01-02 13:24:30 -0800

    Finaly fixing automatic placement

--- constructionManager.js
+++ constructionManager.js
@@ -13,7 +13,7 @@ function FindEmptySites()
     // create a path from the spawn to the energy sources
     const path1 = Game.spawns['Arnice123 First'].pos.findPathTo(Game.flags.SOURCE1.pos)
     const path2 = Game.spawns['Arnice123 First'].pos.findPathTo(Game.flags.SOURCE2.pos)
-    
+
     const constructSitesLength = Game.spawns['Arnice123 First'].room.find(FIND_MY_STRUCTURES, {
     filter: { structureType: FIND_MY_CONSTRUCTION_SITES }})
 
@@ -21,47 +21,43 @@ function FindEmptySites()
     {
         return
     }
-    
-    for(var po in path1)
-    {
-        console.log('this runssssssssssssssssssssss')
-        room.visual.circle(path1[po].pos)
-    } // what I am trying to do but for some reason it doesn't run
-    
+
+    room.visual.poly(path2)
+    room.visual.poly(path1)
 
       // Getting the room terrain
     var terrain = Game.rooms[myHardcodedRoomName].getTerrain()
 
     // for each position in the path there should be a road
-    for (var pos in path1)
+    for (var position in path1)
     {
-       switch(terrain.get(path1[pos].x, path1[pos].y))
+       switch(terrain.get(path1[position].x, path1[position].y))
        {
             // if there is floor or a swamp place a road on that position
             case TERRAIN_MASK_SWAMP:
-                const placeConstructionSiteResult11 = room.createConstructionSite(path1[pos].pos, STRUCTURE_ROAD)
+                const placeConstructionSiteResult11 = room.createConstructionSite(path1[position].pos, STRUCTURE_ROAD)
                 console.log(placeConstructionSiteResult11) // returns -10 (invalid location)
                 //room.createConstructionSite(path1[pos].pos, STRUCTURE_ROAD)
                 break
             case 0:
-                const placeConstructionSiteResult12 = room.createConstructionSite(path1[pos].pos, STRUCTURE_ROAD)
+                const placeConstructionSiteResult12 = room.createConstructionSite(path1[position].pos, STRUCTURE_ROAD)
                  console.log(placeConstructionSiteResult12)
                 break
        }
     }
 
     //same thing as previously
-    for (var pos in path2)
+    for (var position in path2)
     {
-       switch(terrain.get(path2[pos].x, path2[pos].y))
+       switch(terrain.get(path2[position].x, path2[position].y))
        {
             case TERRAIN_MASK_SWAMP:
-                const placeConstructionSiteResult21 = room.createConstructionSite(path2[pos].pos, STRUCTURE_ROAD)
+                const placeConstructionSiteResult21 = room.createConstructionSite(path2[position].pos, STRUCTURE_ROAD)
                  console.log(placeConstructionSiteResult21)
                 //room.createConstructionSite(path2[pos].pos, STRUCTURE_ROAD)
                 break
             case 0:
-                const placeConstructionSiteResult22 = room.createConstructionSite(path2[pos].pos, STRUCTURE_ROAD)
+                const placeConstructionSiteResult22 = room.createConstructionSite(path2[position].pos, STRUCTURE_ROAD)
                  console.log(placeConstructionSiteResult22)
                 //room.createConstructionSite(path2[pos].pos, STRUCTURE_ROAD)
                 break
